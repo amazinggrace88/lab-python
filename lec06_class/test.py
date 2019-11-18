c28b1aec85996fe4d61162703883b448909af3ae
amazinggrace88 <kyungjin2348@gmail.com>
2019-11-18 17:46:44 +0900

    .

---
+++ lec06_class/test.py
@@ -0,0 +1,13 @@
+def test():
+    print('test')
+
+
+def test(param):
+    print('test param =', param)
+
+test()
+
+# overloading:
+# 함수(메소드)의 파라미터가 다른 경우
+# 같은 이름으로 여러개의 함수(메소드)를 정의하는 것.
+# 파이썬 overloading를 제공하지 않음!
