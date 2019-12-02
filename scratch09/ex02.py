cbb9b0169032e7bf07c06d9e7aeac108bde6dd6f
amazinggrace88 <kyungjin2348@gmail.com>
2019-12-02 14:21:28 +0900

    2019-12-02 Update

--- scratch09/ex02.py
+++ scratch09/ex02.py
@@ -6,6 +6,9 @@ import csv
 
 with open('..\\..\\lab_python\\scratch08\\mpg.csv', mode='r', encoding='UTF-8') as f:
     reader = csv.reader(f)
+    reader.__next__()  # 한 줄 읽고 건너 뜀 -> 첫번째 줄은 컬럼 이름들이기 때문에 저장을 건너뛰어 데이터만을 저장한다.
     df = [line for line in reader]
 
 print(df[0:5])
+
+# reader / writer 기억!
