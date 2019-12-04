9a26cf3637ef3c431ec978181724794146c9f43d
amazinggrace88 <35486203+amazinggrace88@users.noreply.github.com>
2019-12-04 11:26:27 +0900

    2019-12-04 Update

---
+++ ex09.py
@@ -0,0 +1,78 @@
+'''
+- emp.csv파일을 읽어서 df생성
+- boolean indexing 사용)
+- 급여(sal)이 2000이상인 직원들의 모든 정보를 출력
+- 부서 번호(deptno)가 10인 직원들의 모든 정보를 출력
+- 급여가 전체 직원 급여의 평균보다 많은 직원의 사번, 이름, 급여(column)를 출력
+- 30번 부서에서 일하는, 직책이 SALESMAN인 직원들의 사번, 이름, 급여, 부서번호를 출력
+- 20, 30번 부서에 근무하는 직원들 중 급여가 2000을 초과하는 직원들의 사번, 이름, 급여, 부서번호를 출력
+- 수당이 없는 직원들 중에서, 매니저가 있고, 직책이 'MANAGER' 또는 'CLERK'인 직원들의 모든 정보를 검색
+- 사원 이름에 'E'가 포함된 직원들의 이름만 출력 (str.contains() 이용)
+- DataFrame을 csv 형식으로 파일에 write하는 함수를 찾아서 실행
+- DataFrame을 csv 형식으로 파일에 write하는 함수를 찾아서 데이터 프레임을 파일로 저장
+
+# df에 먼저 header가 필요없다는 함수를 찾아보고
+# header를 입력
+
+'''
+import pandas as pd
+
+if __name__ == '__main__':
+    file_path = 'emp.csv'
+    # pandas.read_csv() 함수는 csv 파일의 첫번째 줄을 헤더(컬럼 이름)으로 취급.
+    # csv 파일의 첫번째 줄이 헤더(컬럼 이름)이 아니고 실제 레코드인 경우, header=None 파라미터를 추가하자
+    emp = pd.read_csv(file_path, header=None)
+    print('shape = ', emp.shape)  # header = None 일때 : 14 명 / header=None 없으면 13 명(행), 8 명(열)
+    print(emp.head)  # label 을 숫자로 자동 부여했다~
+    # cf. pandas.read_csv() 가 텍스트 편집기보다 빠르기 때문에 텍스트 편집기를 열 때보다 판다스에서 불러오는 게 빠르다.
+
+    # DataFrame 에 컬럼 이름을 설정
+    emp.columns = ['empno', 'ename', 'job', 'mgr', 'hiredate', 'sal', 'comm', 'deptno']
+    print(emp.iloc[0:2])
+
+    # - 급여(sal)이 2000이상인 직원들의 모든 정보를 출력
+    print('\n급여(sal)이 2000이상인 직원들의 모든 정보를 출력')
+    print(emp[emp['sal'] >= 2000])  # df[조건식]
+
+    # - 부서 번호(deptno)가 10인 직원들의 모든 정보를 출력
+    print('\n부서 번호(deptno)가 10인 직원들의 모든 정보를 출력')
+    print(emp[emp['deptno'] == 10])
+
+    # - 급여가 전체 직원 급여의 평균보다 많은 직원의 사번, 이름, 급여(column)를 출력
+    print('\n급여가 전체 직원 급여의 평균보다 많은 직원의 사번, 이름, 급여(column)를 출력')
+    # subset = emp[급여>평균]  # 단계를 나눠서 생각하자 - 코딩!
+    subset = emp[emp['sal'] > emp['sal'].mean()]
+    print('sal mean= ', emp['sal'].mean())
+    print(subset[['empno', 'ename', 'sal']])
+    # sql : select empno, ename, sal from emp where sal > (select avg(sal) from emp);
+
+    # - 30번 부서에서 일하는, 직책이 SALESMAN인 직원들의 사번, 이름, 급여, 부서번호를 출력
+    print('\n30번 부서에서 일하는, 직책이 SALESMAN인 직원들의 사번, 이름, 급여, 부서번호를 출력')
+    c1 = emp['deptno'] == 30  # condition 1
+    c2 = emp['job'] == 'SALESMAN'  # condition 2 - 문자열 비교는 대소문자 구분 잊지 말자~
+    subset = emp[c1 & c2]  # boolean 조건식 - sql의 where 절과 유사함
+    print(subset[['empno', 'ename', 'sal', 'deptno']])  # index에 [[]] 주의해요~
+    # or
+    subset = emp[ (emp['deptno'] == 30) & (emp['job'] == 'SALESMAN')]  # 한 번에 할 때에는 괄호를 지정해서 & 로 묶어주기~
+    # sql : select empno, ename, sal, deptno from emp where deptno == 30 and job == 'SALESMAN';
+
+    # - 20, 30번 부서에 근무하는 직원들 중 급여가 2000을 초과하는 직원들의 사번, 이름, 급여, 부서번호를 출력
+    print('\n20, 30번 부서에 근무하는 직원들 중 급여가 2000을 초과하는 직원들의 사번, 이름, 급여, 부서번호를 출력')
+    # c1 = emp['deptno'] == 20
+    # c2 = emp['deptno'] == 30
+    c1 = emp['deptno'].isin([20, 30])  # oracle in() 연산자와 비슷한 역할: value 를 [list]로 보내주어야 한다.
+    c2 = emp['sal'] > 2000
+    subset = emp[c1 & c2]
+    print(subset[['empno', 'ename', 'sal', 'deptno']])
+    # sql : select empno, ename, sal, deptno from emp where (deptno = 20 or deptno = 30) and sal > 2000;
+    # sql : select empno, ename, sal, deptno from emp where deptno in(20, 30) and sal > 2000;
+
+    # - 수당이 없는 직원들 중에서, 매니저가 있고, 직책이 'MANAGER' 또는 'CLERK'인 직원들의 모든 정보를 검색
+    print('\n수당이 없는 직원들 중에서, 매니저가 있고, 직책이 \'MANAGER\' 또는 \'CLERK\'인 직원들의 모든 정보를 검색')
+    c1 = emp['comm'].isna()  # == np.na 로 비교하면 안된다 why? 비교할 대상 자체가 없다 : .isna() = .isnull() = 컬럼의 값이 없는 컬럼들.(수당이 없는 직원들)
+    # emp['comm'] == np.isna(값) 이면 가능 (?)
+    c2 = ~emp['mgr'].isna()  # not 연산자: '~'
+    # c3 = (emp['job'] == 'MANAGER') | (emp['job'] == 'CLERK') : |를 써야하는 이유 -> 모든 컬럼들을 한 조건씩 비교해야 하기 때문에
+    c3 = emp['job'].isin(['MANAGER', 'CLERK'])
+    subset = emp[c1 & c2 & c3]
+    print(subset)
